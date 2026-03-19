f322082a3ab4c7277f34074ad1bd421fc6234540
BBleagueRL <rayolunabbleague@gmail.com>
2026-03-19 11:50:20 +0100

    Update constants.js

--- assets/js/constants.js
+++ assets/js/constants.js
@@ -2,7 +2,9 @@
 // constants.js
 // Archivo para constantes globales del proyecto
 
+//SHEET DE CONFIG
 export const SHEET_ID = "1O7yVvVJcC2afswIFlYi87J6JCgKaw-Sa18yuRBCoSdI";
+// PAGINAS SHEET
 export const RANKING_SUIZO = "rankingSuizo";
 export const RESULTADOS = "Resultados";
 export const CONFIG = "Config";
@@ -11,5 +13,6 @@ export const USUARIOS_ACEPTADOS = "UsuariosAceptados";
 export const SELECT_EQUIPOS = "SelectEquipos";
 export const TABLA_ORDENADA_EQUIPOS = "TablaOrdenadaEquipos";
 export const TABLA_ORDENADA = "TablaOrdenada";
-export const URL_SCRIPT = "https://script.google.com/macros/s/AKfycbx6XHHZKjrZlgxIJh7znjCwn0W2riM1z-P80Mg1bH6apq5OLsw9TUV9gIm0CKlKSJtyfw/exec";
 export const CONFIG_MENU = "ConfigMenu";
+//APPS SCRIPT (Backend)
+export const URL_SCRIPT = "https://script.google.com/macros/s/AKfycbx6XHHZKjrZlgxIJh7znjCwn0W2riM1z-P80Mg1bH6apq5OLsw9TUV9gIm0CKlKSJtyfw/exec";
